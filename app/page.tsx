048034244c4b13a248092e09e0e6c53a4ec9795e
Martin Stefcek <35243812+Cifko@users.noreply.github.com>
2025-04-08 07:46:56 -0700

    fix: unnecessary scrollbar (#142)

--- app/page.tsx
+++ app/page.tsx
@@ -574,7 +574,7 @@ export default function NetworkStatusPage() {
   }, []);
 
   return (
-    <div className="relative min-h-screen w-full">
+    <div className="relative min-h-full w-full">
       {/* Content */}
       <div className="relative z-10">
         <div className="space-y-4 p-6">
