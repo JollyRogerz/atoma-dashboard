1db810df2486f87072794e4be4228271cb5ad801
Martin Stefcek <35243812+Cifko@users.noreply.github.com>
2025-04-08 06:21:30 -0700

    feat: show unallocated balance (#140)

--- components/credit-balance-card.tsx
+++ components/credit-balance-card.tsx
@@ -6,8 +6,11 @@ import { useSettings } from "@/contexts/settings-context";
 import { useAppState } from "@/contexts/app-state";
 import LoadingCircle from "@/components/LoadingCircle";
 
+const USDC_TO_USD = 1_000_000;
+
 export function CreditBalanceCard({ handleAddFunds }: { handleAddFunds: () => void }) {
   const [balance, setBalance] = useState<string | null>(null);
+  const [freeBalance, setFreeBalance] = useState<string | null>(null);
   const [loggedIn, setLoggedIn] = useState(false);
   const { settings } = useSettings();
   const { state, updateState } = useAppState();
@@ -17,21 +20,24 @@ export function CreditBalanceCard({ handleAddFunds }: { handleAddFunds: () => vo
     setLoggedIn(settings.loggedIn);
     if (!settings.loggedIn) {
       setBalance("-");
+      setFreeBalance("-");
       return;
     }
     try {
       const balancePromise = getBalance();
       const allStacksPromise = getAllStacks();
       const [balanceRes, allStacksRes] = await Promise.all([balancePromise, allStacksPromise]);
-      let partialBalance = allStacksRes?.data.reduce(
-        (acc: number, [stack]: any) =>
-          acc +
-          ((stack.num_compute_units - stack.already_computed_units) / 1000000) *
-            stack.price_per_one_million_compute_units,
-        0
-      );
-      let balance = (balanceRes?.data + partialBalance) / 1000000;
-      setBalance(isNaN(balance) ? "0" : balance.toFixed(2));
+      let lockedBalance =
+        allStacksRes?.data.reduce(
+          (acc: number, [stack]: any) =>
+            acc +
+            ((stack.num_compute_units - stack.already_computed_units) / 1_000_000) *
+              stack.price_per_one_million_compute_units,
+          0
+        ) / USDC_TO_USD;
+      let freeBalance = balanceRes?.data / USDC_TO_USD;
+      setFreeBalance(isNaN(freeBalance) ? "0" : freeBalance.toFixed(2));
+      setBalance(isNaN(freeBalance + lockedBalance) ? "0" : (freeBalance + lockedBalance).toFixed(2));
     } catch (error) {
       console.error("Failed to fetch balance", error);
     }
@@ -49,7 +55,7 @@ export function CreditBalanceCard({ handleAddFunds }: { handleAddFunds: () => vo
   return (
     <Card className="h-[280px] flex flex-col">
       <CardHeader>
-        <CardTitle className="bg-primary-foreground dark:bg-darkMode">Credit Balance</CardTitle>
+        <CardTitle className="text-primary dark:bg-darkMode">Credit Balance</CardTitle>
       </CardHeader>
       <CardContent className="flex-1 flex flex-col justify-between">
         <div className="flex flex-col items-center justify-center flex-1">
@@ -62,7 +68,9 @@ export function CreditBalanceCard({ handleAddFunds }: { handleAddFunds: () => vo
               </div>
             )}
           </div>
-          <div className="text-sm text-muted-foreground mt-2">Available Credits</div>
+          {freeBalance !== null && (
+            <div className="text-sm text-muted-foreground mt-2">Unallocated balance: ${freeBalance}</div>
+          )}
         </div>
         <Button
           className="w-full bg-primary hover:bg-secondary-foreground text-base"
