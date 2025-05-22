022663c3e8728383cdb4a2d542c0f1eecbd229d3
Jorge António <matroid@outlook.com>
2025-05-22 14:10:44 +0100

    Revert "Feature: Comprehensive Optimizations, Testing Foundation, & UI Refine…" (#173)
    This reverts commit d3df5b7804ea4967c16b34ebde1ec0c15983c5f3.

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -25,7 +25,7 @@ import LoadingCircle from "../../components/LoadingCircle";
 import { useSettings } from "../../contexts/settings-context";
 import ReactMarkdown from "react-markdown";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { ModelModality } from "@/lib/atoma-types";
+import { ModelModality } from "@/lib/atoma";
 
 export type ModelCategories = "chat" | "embeddings" | "images";
 
