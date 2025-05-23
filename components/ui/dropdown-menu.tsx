615dfac3d685731062b5fb96c233a182534418c4
Jorge António <matroid@outlook.com>
2025-05-23 10:17:13 +0100

    Revert "Feat: Comprehensive Optimizations, Testing Foundation, & UI Refinemen…" (#176)
    This reverts commit a857913de7ff5d745888598593dac98a8a3bf066.

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -47,7 +47,6 @@ const DropdownMenuSubContent = React.forwardRef<
     ref={ref}
     className={cn(
       "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg animate-in fade-in-0 zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-      "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg animate-in fade-in-0 zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
     )}
     {...props}
